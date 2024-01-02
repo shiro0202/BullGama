fe73ad20025f986f6793fe7a919e0f02ded130c9
shiro0202 <123615196+shiro0202@users.noreply.github.com>
2024-01-02 13:55:31 +0900

    player
    move

--- Assets/Scripts/PlayerMV.cs
+++ Assets/Scripts/PlayerMV.cs
@@ -4,28 +4,15 @@ using UnityEngine;
 
 public class PlayerMV : MonoBehaviour
 {
-    public float MaxSpd;
+    public float speed = 0.5f;
 
-    Rigidbody2D rigid;
-
-    void Awake()
-    {
-        rigid = GetComponent<Rigidbody2D>();
-    }
-
-    void FixedUpdate()
+    private void Update()
     {
         float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        Vector3 curPos = transform.position;
+        Vector3 nextPos = new Vector3(h, v, 0) * speed * Time.deltaTime;
 
-        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
-
-        if(rigid.velocity.x > MaxSpd)
-        {
-            rigid.velocity = new Vector2(MaxSpd, rigid.velocity.y);
-        }
-        else if(rigid.velocity.x < MaxSpd * (-1))
-        {
-            rigid.velocity = new Vector2(MaxSpd * (-1), rigid.velocity.y);
-        }
+        transform.position = curPos + nextPos;
     }
 }
